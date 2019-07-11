4c005fe172d9512f57a3c690665148a9cb8853ba
Trevor Thompson <Thompson_Trevor@bah.com>
2019-07-11 15:00:22 -0400

    Added regular expression escape to handle multiple different whitespace/newline characters, now all tests pass

--- main.js
+++ main.js
@@ -1,22 +1,11 @@
 function songDecoder(song){
   // ...
     countOfWubApperance = (song.match(/WUB/g,) || []).length;
-      console.log(countOfWubApperance);
-        
-	  if(countOfWubApperance => 1 && song.length > 3)
-	    {
-	         originalSong = song.replace(/WUB/g, ' ');
-		      //originalSong = song.replace(/  +/g, ' ' );
-		           return originalSong.trim();
-			     }
-			       /*else if (song.length <= 3)
-			         {
-				      song.trim();
-				           return song;
-					     }*/
-					       /*else if(countOfWubApperance > 1)
-					         {
-						      originalSong= song.replace(/WUB/, ' ');
-						           return originalSong;
-							     }*/
-							     }
+      
+        if(countOfWubApperance => 1 && song.length > 3)
+	  {
+	       originalSong = song.replace(/WUB/g, ' ');
+	            originalSong = originalSong.replace(/\s\s+/g, ' ');
+		         return originalSong.trim();
+			   }
+			   }
